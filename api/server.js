d22792aec77e9a4ee941c91508b0fdd57d8e0489
adryanmaurentebg005-ui <adryanmaurente.bg005@academico.ifsul.edu.br>
2025-12-04 04:02:23 -0300

    tst

--- api/server.js
+++ api/server.js
@@ -14,9 +14,9 @@ const app = express();
 const PORT = 3111;
  
 app.set('view engine', 'ejs'); 
-app.set("views", path.join(process.cwd(), "views"));
+app.set("views", path.join(__dirname, "..", "views"));
  
-app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
 app.use(methodOverride('_method'));
